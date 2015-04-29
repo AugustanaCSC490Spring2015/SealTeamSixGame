7de5146e66ef842a11c39a29d16de42e338b20c3
Dan Shultz <dan@danshultz.com>
2015-04-29 17:30:47 -0500

    Added a dialog to prompt the user if they are sure they want to quit the game when the press the back button

--- app/src/main/java/edu/augustana/csc490/circleofdeath/GameActivity.java
+++ app/src/main/java/edu/augustana/csc490/circleofdeath/GameActivity.java
@@ -1,10 +1,10 @@
 package edu.augustana.csc490.circleofdeath;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * GameActivity sets the layout for the game
@@ -20,10 +20,30 @@ public class GameActivity extends Activity {
         setContentView(R.layout.activity_game);
     }
 
+    /**
+     * onBackPressed() is called when the user hits the back key on their device. We override it
+     * after the game has started to stop them from going back into MenuActivity. When the users hit
+     * the back button, it prompts them with a dialog asking them to confirm if they want to go back
+     * to the main menu and lose all current game data or cancel and continue the game.
+     */
     @Override
     public void onBackPressed() {
-        // do nothing - overriding this method stops the player from being able to use the back
-        // button to go back to the MenuActivity
-        // TODO: Prompt user if they are sure they want to quit the game and go back to the menu
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.caution);
+        builder.setMessage(R.string.quit_game_warning);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(GameActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Do nothing, dialog will close and game continues
+            }
+        });
+        builder.show();
     }
 }
